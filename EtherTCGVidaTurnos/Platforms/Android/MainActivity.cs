bf8416ae2f4c55ea3831e5133a2ea799830d190a
LucioLrj <lucio.laranjeira@outlook.com>
2025-09-29 14:37:16 -0300

    1.0.4 - Icones novos

--- EtherTCGVidaTurnos/Platforms/Android/MainActivity.cs
+++ EtherTCGVidaTurnos/Platforms/Android/MainActivity.cs
@@ -7,5 +7,15 @@ namespace EtherTCGVidaTurnos
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                // Troque "#000000" pela cor que quiser (hex)
+                Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#000000"));
+            }
+        }
     }
 }
